ac97546f67d10ca370e62055266ec4817f1f1e69
Dhiraj <DK266107@gmail.com>
2020-11-27 10:25:10 +0530

    IntInput Changes to IntOnly

---
+++ TestCode/TestCode/IntOnly.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int intOnly(string);
+
+int main() {
+
+	string str;
+
+	cout << "Enter a number:\n";
+	cin >> str;
+
+	int n = intOnly(str);
+
+	while (n == 1) {
+		cout << "Opps!!! You have entered a wrong input\n";
+		cout << "Please enter only integer values:\n";
+		cin >> str;
+		n = intOnly(str);
+	}
+
+	if (n == 0)
+		cout << "Thanks" << endl;
+
+	return 0;
+}
+int intOnly(string str) {
+	int flag = 0;
+	for (int i = 0; i < str.length(); i++) {
+
+		if (!isdigit(str[i])) {
+			flag = 1;
+			break;
+		}
+	}
+	return flag;
+}
